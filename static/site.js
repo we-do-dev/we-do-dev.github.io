8ea38492de22f6386ae90618d98a3799515e017b
d2weber <29163905+d2weber@users.noreply.github.com>
2022-04-16 15:31:38 +0200

    Close burger menu on click2

--- static/site.js
+++ static/site.js
@@ -3,23 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const navbarBurgers = document.querySelectorAll('.navbar-burger');
     const navbarItems = document.querySelectorAll(".navbar-item");
 
-    function toggleBurger(burger_el) {
-        // Get the target from the "data-target" attribute
-        const target = burger_el.dataset.target;
-        const $target = document.getElementById(target);
-
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        burger_el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-    }
-
     navbarBurgers.forEach(burger_el => {
         burger_el.addEventListener('click', () => {
-            toggleBurger(burger_el)
+            // Toggle burger-menu
+            document.getElementById(burger_el.dataset.target).classList.toggle('is-active');
+            $target.classList.toggle('is-active');
         });
         navbarItems.forEach(item => {
             item.addEventListener("click", () => {
-                toggleBurger(burger_el)
+                // Close burger-menu
+                document.getElementById(burger_el.dataset.target).classList.remove('is-active');
+                $target.classList.remove('is-active');
             });
         });
     });
